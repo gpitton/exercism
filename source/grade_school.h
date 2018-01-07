a4c3cae7ec8b2ec4cdf5384d42e8592e28cea907
Giuseppe Pitton <gpitton@sissa.it>
2018-01-07 13:00:11 +0100

    new exercise requiring C++17

---
+++ source/grade_school.h
@@ -0,0 +1,23 @@
+#if !defined(GRADE_SCHOOL_H)
+#define GRADE_SCHOOL_H
+#include <map>
+#include <string>
+#include <vector>
+
+
+namespace grade_school
+{
+
+class school
+{
+  public:
+      std::map<int,std::vector<std::string>> roster();
+      void add(const std::string,int);
+      std::vector<std::string> grade(int);
+  private:
+      std::map<int,std::vector<std::string>> sort_roster();
+      std::map<int,std::vector<std::string>> school_roster {};
+};
+
+}
+#endif
